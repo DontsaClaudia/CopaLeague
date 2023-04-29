80f0a84e9250fa00d2bd8e7473ba9e634e0abe5f
DontsaClaudia <donstsaclaudia@gmail.com>
2023-04-29 06:30:39 +0200

    correction et ajout

--- app/Http/Controllers/Traits/MediaUploadingTrait.php
+++ app/Http/Controllers/Traits/MediaUploadingTrait.php
@@ -1,12 +1,48 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Traits;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
-class Controller extends BaseController
+trait MediaUploadingTrait
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function storeMedia(Request $request)
+    {
+        // Validates file size
+        if (request()->has('size')) {
+            $this->validate(request(), [
+                'file' => 'max:' . request()->input('size') * 1024,
+            ]);
+        }
+        // If width or height is preset - we are validating it as an image
+        if (request()->has('width') || request()->has('height')) {
+            $this->validate(request(), [
+                'file' => sprintf(
+                    'image|dimensions:max_width=%s,max_height=%s',
+                    request()->input('width', 100000),
+                    request()->input('height', 100000)
+                ),
+            ]);
+        }
+
+        $path = storage_path('tmp/uploads');
+
+        try {
+            if (! file_exists($path)) {
+                mkdir($path, 0755, true);
+            }
+        } catch (\Exception $e) {
+        }
+
+        $file = $request->file('file');
+
+        $name = uniqid() . '_' . trim($file->getClientOriginalName());
+
+        $file->move($path, $name);
+
+        return response()->json([
+            'name'          => $name,
+            'original_name' => $file->getClientOriginalName(),
+        ]);
+    }
 }
